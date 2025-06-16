b43e9dbd6781c9a038b416499dc07b3cb40ddc15
Prakash Parmar <parmarprakashr60@gmail.com>
2025-06-16 22:05:44 +0900

    my_realloc - basic function complete

--- C/project/custom_memory_allocator/main.c
+++ C/project/custom_memory_allocator/main.c
@@ -2,6 +2,7 @@
  #include<stdint.h>
  #include<stdatomic.h>
  #include<string.h>
+ #include<math.h>
 
  #define HEAP_SIZE (1024 * 1024 * 150)
  
@@ -28,10 +29,11 @@
 void print_block_metadata(BlockHeader* metaBlock){
 	
 	printf("\n");	
-	printf("%zu \n", metaBlock->size);
-	printf("%d \n", metaBlock->is_free);
-	printf("%p \n", metaBlock->next);
-	printf("%p \n", metaBlock->prev);
+	printf("Address: %p\n", metaBlock);
+	printf("size: %zu \n", metaBlock->size);
+	printf("is_free: %d \n", metaBlock->is_free);
+	printf("next: %p \n", metaBlock->next);
+	printf("prev: %p \n", metaBlock->prev);
 	printf("\n");
 
 	return;
@@ -144,6 +146,42 @@ void print_block_metadata(BlockHeader* metaBlock){
 			return ptr;
 		}
 	}
+
+	// grow the block
+	
+	//first check if next block is free and has enough size
+	if(block_before_resize->next && block_before_resize->next->is_free && block_before_resize->next->size >= new_total_required_size - block_before_resize->size){
+		block_before_resize->size += block_before_resize->next->size;
+		block_before_resize->next = block_before_resize->next->next;
+		
+		//printf("Checkpoint 3\n");
+
+		if(block_before_resize->next){
+			block_before_resize->next->prev = block_before_resize;
+		}
+
+
+		//printf("Checkpoint 4\n");
+		return ptr;
+	}
+
+	//if the next block is not big enough to satisfy bytes size
+	// go through the linked list starting from head and find if there any block exist with sufficient size 
+	void *new_ptr = (void *)my_malloc(bytes);
+	if(new_ptr == NULL)
+			printf("Out of memory!");
+	else{
+		// we found the block that is big enough to store total bytes
+		//copy the old block content to new block
+		size_t bytes_to_copy = fmin(bytes, block_before_resize->size - sizeof(BlockHeader));
+	   	memcpy(new_ptr, ptr, bytes_to_copy);
+
+		// free old block poinint by ptr
+		my_free(ptr);
+		return new_ptr;	
+	}
+	
+	return NULL;
  } 
 
  int main(int arc, char* const* arv) {
@@ -155,11 +193,19 @@ void print_block_metadata(BlockHeader* metaBlock){
     char* second_name = (char *)my_malloc(7);  // 6 chars + null terminator
     strcpy(second_name, "parmar");
 
-    printf("starting add is %p and value is %s\n", first_name, first_name);
-    printf("starting add is %p and value is %s\n", second_name, second_name);
-
-    printf("byte diff = %zu\n", second_name - first_name);
+	printf("First checkpoint \n");
 	
+	first_name = (char *)my_realloc(first_name, 25);
+	
+	printf("Second Checkpoint \n");
+	strcpy(first_name, "Prakash Parmar family");
+    //printf("starting add is %p and value is %s\n", first_name, first_name);
+    //printf("starting add is %p and value is %s\n", second_name, second_name);
+
+    //printf("byte diff = %zu\n", second_name - first_name);
+	
+	//printf("----");
+
 	my_free(first_name);
 	my_free(second_name);
 
