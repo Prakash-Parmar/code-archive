eb431173b93fa9b02c7464db43dc504836918e6e
Prakash Parmar <parmarprakashr60@gmail.com>
2025-09-13 16:10:13 +0900

    custom memory allocator with LIFO functionality complete.

--- C/project/custom_memory_allocator/StackBasedAllocator/stackAllocator.h
+++ C/project/custom_memory_allocator/StackBasedAllocator/stackAllocator.h
@@ -3,6 +3,7 @@
 
 #include<stdio.h>
 #include<stdint.h>
+#include<stdbool.h>
 
 typedef struct Stack Stack;
 
@@ -23,6 +24,14 @@ bool is_power_of_two(uintptr_t ptr);
 
 void stack_init(Stack *s, void *backing_buffer, size_t backing_buffer_length);
 size_t calculate_padding_with_header(uintptr_t ptr, uintptr_t alignment, size_t header_size);
+
+void *stack_alloc_align(Stack* s, size_t size, size_t alignment);
+void *stack_alloc(Stack* s, size_t size);
+
+void stack_free(Stack* s, void *ptr);
+void *stack_resize_align(Stack *s, void *ptr, size_t old_size, size_t new_size, size_t alignment);
+void *stack_resize(Stack *s, void *ptr, size_t old_size, size_t new_size);
+void stack_free_all(Stack *s);
 
 #endif
 
